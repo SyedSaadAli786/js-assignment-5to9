890a014b7217e45f859099a38decf756c14739f9
Syed Saad Ali <saadali23489@gmail.com>
2022-05-24 20:55:27 +0500

    marksheet

---
+++ app.js
@@ -0,0 +1,86 @@
+
+/*Qno 1 (a)
+ 
+let a = 10
+document.write("the valu of a is: " + a);
+
+Qno 1 (b) 
+
+let a = 10
+document.write("the value of ++a is: "  + ++a + " " + ++a + " " + --a + " " + a-- );
+
+
+//Qno 2 (a)  
+
+let a = 2
+let b = 1
+
+
+//console.log( --a - --b + ++b + b--)
+// ANSWER :  3
+
+
+//Qno 2 (b)
+//console.log(--a)
+// ANSWER :  1
+
+
+//Qno 2 (c)
+//console.log(--a - --b)
+// ANSWER :  1
+
+
+//Qno 2 (d)
+//console.log(--a - --b + ++b)
+// ANSWER :  2
+
+
+//Qno 2 (e)
+console.log(--a - --b + ++b + b--)
+// ANSWER :  3
+
+
+
+//Qno 3 (a) 
+
+let name = prompt("enter your name", "Write here");
+document.write( "hello " + name)
+
+//Qno 5 
+
+
+let num = prompt("which table you want", "enter here");
+
+document.write(num + " X  1 = " + num*1);
+document.write(num + " X  2 = " + num*2);
+document.write(num + " X  3 = " + num*3);
+document.write(num + " X  4 = " + num*4);
+document.write(num + " X  5 = " + num*5);
+document.write(num + " X  6 = " + num*6);
+document.write(num + " X  7 = " + num*7);
+document.write(num + " X  8 = " + num*8);
+document.write(num + " X  9 = " + num*9);
+document.write(num + " X  10 = " + num*10);
+
+
+
+//Qno 6 */
+
+
+let sub1  = promt("enter your 1st subject");
+let sub2 = promt("enter your 2nd subject");
+let sub3 = promt("enter your 3rd subject");
+let total = 100;
+let sub01 = promt("enter " + sub1 + " marks");
+let sub02 = promt("enter " + sub2 + " marks");
+let sub03 = promt("enter " + sub3 + " marks");
+let per1 = sub1/total*100
+
+
+
+
+
+
+
+
+
